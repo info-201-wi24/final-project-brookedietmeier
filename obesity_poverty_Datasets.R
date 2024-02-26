1a3ea2d2ac82f1eb0e2067151ccceb496dcc1a20
brookedietmeier <bdietm@uw.edu>
2024-02-26 14:29:39 -0800

    MOST UPDATED

--- obesity_poverty_Datasets.R
+++ obesity_poverty_Datasets.R
@@ -3,39 +3,41 @@ library(dplyr)
 library(stringr)
 
 # Loads in your datasets
-obesity_df <- read.csv("/Users/brookedietmeier/Desktop/Info_201_joining_datasets/final-project-brookedietmeier/2022-white.csv") 
-poverty_df <- read.csv("/Users/brookedietmeier/Desktop/Info_201_joining_datasets/final-project-brookedietmeier/poverty_state_grid_table.csv", skip = 3, header = TRUE)
-nutrition_df <- read.csv("/Users/brookedietmeier/Desktop/Info_201_joining_datasets/final-project-brookedietmeier/Nutrition__Physical_Activity__and_Obesity_-_Behavioral_Risk_Factor_Surveillance_System.csv")
+obesity_df <- read.csv("2022-white.csv") 
+poverty_df <- read.csv("poverty_state_grid_table.csv", skip = 3, header = TRUE)
+nutrition_df <- read.csv("Nutrition__Physical_Activity__and_Obesity_-_Behavioral_Risk_Factor_Surveillance_System.csv")
 
-#changed the dataset to be from 2020-2022 to match other datasets
+#nutrition 
 nutrition_df <- nutrition_df %>% 
-  filter(YearStart == "2020" | YearStart == "2021" | YearStart == "2022")
+  filter(YearStart == 2020 | YearStart == 2021 | YearStart == 2022) %>%
+  select(YearStart, LocationDesc, Question, Data_Value) 
 
-#keep all columns up to the one just before "Margin of Error"
-poverty_df <- poverty_df %>%
-              select(1:which(names(poverty_df) == "Margin.of.error1......") - 1)
-
-obesity_df <- obesity_df %>%
-             select(1:which(names(obesity_df) == "X95..CI") - 1)
+nutrition_df <- nutrition_df %>%
+  filter(Question == "Percent of adults who engage in no leisure-time physical activity")
 
 #change name of nutrition_df column from 'LocationDesc' to 'State'
 nutrition_df <- nutrition_df %>% 
-                rename(State = LocationDesc)
+  rename(State = LocationDesc)
 
-#join datasets
-# First join poverty_df and obesity_df
-poverty_obesity_df <- inner_join(poverty_df, obesity_df, by = "State")
+#keep all columns up to the one just before "Margin of Error"
+poverty_df <- poverty_df %>%
+  select(1:which(names(poverty_df) == "Margin.of.error1......") - 1)
 
-combined_df <- inner_join(poverty_obesity_df, nutrition_df, by = "State")
+obesity_df <- obesity_df %>%
+  select(1:which(names(obesity_df) == "X95..CI") - 1)
 
+#join poverty and obesity datasets
+obesity_poverty_df <- inner_join(poverty_df, obesity_df, by = "State")
 
 #rename column names to properly identify data in joined dataset 
-combined_df <- combined_df %>% 
-                      rename(`Poverty Rate` = Rate)
+obesity_poverty_df <- obesity_poverty_df %>% 
+  rename(`Poverty Rate` = Rate)
 
-combined_df <- combined_df %>% 
-                      rename(`Obesity Prevelance` = Prevalence)
+obesity_poverty_df <- obesity_poverty_df %>% 
+  rename(`Obesity Prevelance` = Prevalence)
+
+combined_df <- inner_join(obesity_poverty_df, nutrition_df, by = "State")
 
 #save unified dataset to a new file 
-write.csv(combined_df, "Unified_dataset.csv", row.names = FALSE)
+write.csv(obesity_poverty_df, "Unified_dataset.csv", row.names = FALSE)
 
